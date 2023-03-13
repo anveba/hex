f44048ccca191c95171c71d10f76bc86a6b47723
OliverSiggaard <s204450@student.dtu.dk>
2023-03-13 17:34:54 +0100

    Swap Rule and corresponding tests implemented😁

--- src/test/hex/PlayerTest.java
+++ src/test/hex/PlayerTest.java
@@ -7,7 +7,8 @@ import main.hex.*;
 
 public class PlayerTest {
 
-    private Player player1;
+    private Player player1, player2;
+    private GameLogic gameLogic;
 
     @Before
     public void setup() {
@@ -21,9 +22,12 @@ public class PlayerTest {
 
     @Test
     public void getPlayerColour_player1BlueColour_colourReturned() {
-        assertEquals(Tile.Colour.BLUE, player1.getPlayerColor());
+        assertEquals(Tile.Colour.BLUE, player1.getPlayerColour());
     }
 
-
-
+    @Test
+    public void setPlayerColour_player1RedColour_player1HasRedColour() {
+        player1.setPlayerColour(Tile.Colour.RED);
+        assertEquals(Tile.Colour.RED, player1.getPlayerColour());
+    }
 }
